f883cc92a4163610b8d84bb743185551f8232efe
David Alienyi <alienyidavid4christ@gmail.com>
2021-01-08 19:54:19 +0100

    fix coonfig bug

--- src/Config/DBConfig.php
+++ src/Config/DBConfig.php
@@ -15,11 +15,11 @@
                 'user' => 'ammasng_amass',
                 'password' => 'weareamass123',
                 'host' => 'localhost',
-                'database' => 'ammassng_dev',
+                'database' => 'ammasng_dev',
                 'port' => 8889
             ),
             'production' => array(
-                
+
             )
         );
     }
