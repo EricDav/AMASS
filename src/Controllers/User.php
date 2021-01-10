fc646e71a38e6c3b428a86e6558fb41f6257912d
David Alienyi <alienyidavid4christ@gmail.com>
2021-01-10 22:58:26 +0100

    update response object

--- src/Controllers/User.php
+++ src/Controllers/User.php
@@ -14,7 +14,7 @@
         public function create($request) {
             $this->dbConnection->open();
             $errorMessages = $this->validateCreate($request);
-            if (sizeof($errorMessages) == 0) {
+            if ($errorMessages == null) {
                 $passwordHash = password_hash($request->body->password, PASSWORD_DEFAULT);
                 $userDetails = array(
                     'email' => $request->body->email,
@@ -40,51 +40,51 @@
                         'name' => $request->body->first_name,
                     );
                     $jwt = JWT::generateJWT(json_encode(['email' => $request->body->email, 'name' => $request->body->name, 'id' => $userId, 'exp' => (time()) + User::EXP_IN_SEC]));
-                    $this->jsonResponse(array('success' => true, 'message' => 'User created successfully', 'token' => $jwt, 'user' => $user, 'exp' => User::EXP_IN_SEC), Controller::HTTP_OKAY_CODE);
+                    $this->jsonResponse(array('success' => '11', 'message' => 'User created successfully', 'token' => $jwt, 'user' => $user, 'exp' => User::EXP_IN_SEC), Controller::HTTP_OKAY_CODE);
                 }
 
-                $this->jsonResponse(array('success' => false, 'message' => 'Server error'));
+                $this->jsonResponse(array('success' => '00', 'message' => 'Server error'));
             } else {
-                $this->jsonResponse(array('success' => false, 'message' => $errorMessages));
+                $this->jsonResponse(array('success' => '00', 'message' => $errorMessages));
             }
         }
 
         public function validateCreate($request) {
-            $errorMessages = array();
+            $errorMessages = null;
             $name = $request->body->name;
 
             $isValidName = Helper::isValidName($name);
             $isValidEmail = Helper::isValidEmail($request->body->email);
 
             if (!$isValidEmail['isValid']) {
-                $errorMessages['email'] = $isValidEmail['message'];
+                return $isValidEmail['message'];
             }
 
             if (!$isValidName['isValid']) {
-                $errorMessages['name'] = $isValidName['message'];
+                return $isValidName['message'];
             }
 
             if (!trim($request->body->password)) {
-                $errorMessages['password'] = 'Password is required';
+                return 'Password is required';
             }
 
             if (strlen($request->body->phone_number) != 11 || !is_numeric($request->body->phone_number)) {
-                $errorMessages['phone_number'] = 'Invalid phone number';
+                return'Invalid phone number';
             }
 
             if ($request->body->role != 0 && $request->body->role != 1) {
-                $errorMessages['role'] = 'Invalid role';
+                return 'Invalid role';
             }
 
             if (sizeof($errorMessages) == 0) {
                 $user = Model::findOne($this->dbConnection, array('email' => $request->body->email), 'users');
 
                 if ($user) {
-                    $errorMessages['email'] = 'User with this email already exist';
+                    return 'User with this email already exist';
                 } else {
                     $user = Model::findOne($this->dbConnection, array('phone_number' => $request->body->phone_number), 'users');
                     if ($user) {
-                        $errorMessages['phone_number'] = 'User with this phone number already exist';
+                        return 'User with this phone number already exist';
                     }
                 }
 
