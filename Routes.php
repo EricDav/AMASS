4b5a5f84c5852a34fbde32d481dc94d627336685
David Alienyi <alienyidavid4christ@gmail.com>
2021-03-02 15:03:07 +0100

    f

--- Routes.php
+++ Routes.php
@@ -11,13 +11,13 @@ class AMASS {
             BASE_URL . '/products' => 'Product@create',
             BASE_URL . '/comments' => 'Product@addComment',
             BASE_URL . '/appointments' => 'User@bookAppointment',
+            BASE_URL . '/all/products' => 'Product@get',
+            BASE_URL . '/s/products' => 'Product@getProduct',
+            BASE_URL . '/s/users' => 'User@getUser',
+            BASE_URL . '/products/comments' => 'Product@getComments',
         ),
 
         'GET' => array(
-            BASE_URL . '/products' => 'Product@get',
-            BASE_URL . '/s/products' => 'Product@getProduct',
-            BASE_URL . '/comments' => 'Product@getComments',
-            BASE_URL . '/s/users' => 'User@getUser',
         )
     ];
 
@@ -28,13 +28,14 @@ class AMASS {
             BASE_URL . '/verify' => ['email', 'phone_number', 'name'],
             BASE_URL . '/products' => ['token', 'name', 'description', 'category_id', 'price', 'file'],
             BASE_URL . '/comments' => ['product_id', 'comment', 'rate', 'hash'],
-            BASE_URL . '/appointments' => ['product_id', 'hash']
+            BASE_URL . '/appointments' => ['product_id', 'hash'],
+            BASE_URL . '/all/products' => ['page_num'],
+            BASE_URL . '/s/products' => ['product_id'],
+            BASE_URL . '/s/users' => ['user_id'],
+            BASE_URL . '/products/comments' => ['product_id'],
         ),
         'GET' => array(
-            BASE_URL . '/products' => ['page_num'],
-            BASE_URL . '/comments' => ['product_id'],
-            BASE_URL . '/s/products' => ['product_id'],
-            BASE_URL . '/s/users' => ['user_id']
+            
         )
     ];
 }
